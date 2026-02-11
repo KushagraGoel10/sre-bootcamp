8389651d68ae1bd6dd006c6d28a82dad09cd6efb
Kushagra <goelkushagra2000@gmail.com>
2026-02-12 00:52:18 +0530

    latest changes

--- sre-ex1/routes/studentRoutes.js
+++ sre-ex1/routes/studentRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const service = require("../services/studentService");
 
+// For getting all students
 router.get("/", (req, res) => {
   res.json(service.getAllStudents());
 });
 
+// student by id
 router.get("/:id", (req, res) => {
   const student = service.getStudentById(parseInt(req.params.id));
 
@@ -15,6 +17,7 @@ router.get("/:id", (req, res) => {
   res.json(student);
 });
 
+//adding a new student
 router.post("/", (req, res) => {
   if (!req.body.name)
     return res.status(400).json({ error: "Name is required" });
@@ -23,6 +26,7 @@ router.post("/", (req, res) => {
   res.status(201).json(student);
 });
 
+//updating existing student
 router.put("/:id", (req, res) => {
   const student = service.updateStudent(
     parseInt(req.params.id),
@@ -35,6 +39,7 @@ router.put("/:id", (req, res) => {
   res.json(student);
 });
 
+//deleting a student
 router.delete("/:id", (req, res) => {
   const deleted = service.deleteStudent(parseInt(req.params.id));
 
